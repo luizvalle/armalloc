33cacbcc87feb8bf73ef537e1f7534063ba0086a
Luiz do Valle <lf.lemos.valle@gmail.com>
2025-08-03 20:11:24 -0400

    Remove references to the write syscall

--- src/mm.s
+++ src/mm.s
@@ -1,15 +1,5 @@
 .include "sys_macros.inc"
 
-.section .data
-
-not_implemented_txt: .asciz "Not implemented.\n"
-
-// Compute the size of the string (including the null terminator)
-// '.': current location counter
-// '. - not_implemented_txt': difference between current address and label
-// This sets 'not_implemented_txt_size' to the number of bytes in the string
-.equ not_implemented_txt_size, . - not_implemented_txt
-
 .section .text
 
 .global mm_init
@@ -19,20 +9,16 @@ not_implemented_txt: .asciz "Not implemented.\n"
 
 // Initializes the memory manager.
 mm_init:
-    sys_write #1, not_implemented_txt, #not_implemented_txt_size
     ret
 
 // De-initializes the memory manager.
 mm_deinit:
-    sys_write #1, not_implemented_txt, #not_implemented_txt_size
     ret
 
 // Allocates a block with at least size bytes of payload.
 mm_malloc:
-    sys_write #1, not_implemented_txt, #not_implemented_txt_size
     ret
 
 // Frees a block
 mm_free:
-    sys_write #1, not_implemented_txt, #not_implemented_txt_size
     ret
